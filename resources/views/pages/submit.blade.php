8af3d5118ab6548333e8c261f63530bc8d7fd403
Todd Austin <austin.todd.j@gmail.com>
2017-04-17 10:16:22 -0500

    Adds submission input form

--- resources/views/pages/submit.blade.php
+++ resources/views/pages/submit.blade.php
@@ -8,7 +8,31 @@
                     <div class="panel-heading">Submit</div>
 
                     <div class="panel-body">
-                        <p><i class="fa fa-fw fa-check-square-o"></i> Form to submit a new link here</p>
+                        <form class="form-horizontal" role="form" method="POST" action="#">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="link" class="col-md-4 control-label">Video URL</label>
+
+                                <div class="col-md-6">
+                                    <input id="link" type="text" class="form-control" name="email" value="{{ old('link') }}" required>
+                                    <p class="small text-muted">Example: <code>http://vimeo.com/22439234</code></p>
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('link') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
