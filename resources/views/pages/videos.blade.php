8904359df8bb7ff2ffacd7885109a0ff8ff27a99
Todd Austin <austin.todd.j@gmail.com>
2017-04-24 15:03:09 -0500

    Updates tests

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -6,10 +6,11 @@
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">Videos</div>
-
                     <div class="panel-body">
                         @foreach($data->data as $video)
-                            <a href="{{ $video->attributes->url }}" target="_blank"><h4><i class="fa fa-fw fa-youtube-play"></i> {{ $video->attributes->title }}</h4></a>
+                            <a href="{{ $video->attributes->url }}" target="_blank">
+                                <h4><i class="fa fa-fw fa-youtube-play"></i> {{ $video->attributes->title }}</h4>
+                            </a>
                             <hr>
                         @endforeach
                     </div>
