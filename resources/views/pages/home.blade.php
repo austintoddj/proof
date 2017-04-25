a34379172aafcb13521fe7730a850c862d36289d
Todd Austin <austin.todd.j@gmail.com>
2017-04-25 10:52:50 -0500

    Update the trending sections

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,15 +5,17 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Trending Now</div>
+                <div class="panel-heading"><i class="fa fa-fw fa-fire"></i> Trending Now</div>
 
                 <div class="panel-body">
                     <div class="col-md-6">
-                        <h3>Top Videos by Views</h3>
+                        <h3>Top 10 by Views</h3>
+                        <hr>
                         {{ var_dump($trendingVideosByViews) }}
                     </div>
                     <div class="col-md-6">
-                        <h3>Top Videos by Votes</h3>
+                        <h3>Top 10 by Votes</h3>
+                        <hr>
                         {{ var_dump($trendingVideosByVotes) }}
                     </div>
                 </div>
