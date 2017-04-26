9167936b15a2b1d0f79d2415d2dbf01b8cc66e06
Todd Austin <austin.todd.j@gmail.com>
2017-04-26 16:22:43 -0500

    Embed videos with VideoUrlParser

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -9,18 +9,22 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">All Videos ({{ sizeof($data) }})</div>
                     <div class="panel-body">
-                        @foreach($data as $video)
-                            <p><a href="{{ $video->attributes->url }}" target="_blank"> {{ $video->attributes->title }}</a> <span class="text-muted small"><i class="fa fa-fw fa-eye"></i> {{ $video->attributes->view_tally }}&nbsp;&nbsp;<i class="fa fa-fw fa-check"></i> {{ $video->attributes->vote_tally }}</span></p>
-                            <form role="form" method="POST" action="{{ route('videos.store') }}">
-                                {{ csrf_field() }}
-                                <input type="hidden" name="_videoId" value="{{ $video->id }}">
-                                <input type="hidden" name="_userId" value="{{ Auth::user()->id }}">
-                                <input type="hidden" name="_userIp" value="{{ $_SERVER['REMOTE_ADDR'] }}">
-                                <button class="btn btn-default btn-xs" type="submit" name="_opinion" value="1" @if(\App\Models\Vote::votesLeft() == 0) disabled @endif><i class="fa fa-fw fa-thumbs-up"></i> Upvote</button>
-                                <button class="btn btn-default btn-xs" type="submit" name="_opinion" value="-1" @if(\App\Models\Vote::votesLeft() == 0) disabled @endif><i class="fa fa-fw fa-thumbs-down"></i> Downvote</button>
-                            </form>
-                            <hr>
-                        @endforeach
+                        <div class="col-md-6">
+                            @foreach($data as $video)
+                                <iframe src="{{ \App\Helpers\VideoUrlParser::get_url_embed($video->attributes->url) }}" style="width: 100%; height: 225px" frameborder="0" allowfullscreen></iframe>
+                                <p><a href="{{ $video->attributes->url }}" target="_blank"> {{ $video->attributes->title }}</a> <span class="text-muted small pull-right"><i class="fa fa-fw fa-eye"></i> {{ $video->attributes->view_tally }}&nbsp;&nbsp;<i class="fa fa-fw fa-check"></i> {{ $video->attributes->vote_tally }}</span></p>
+
+                                <form role="form" method="POST" action="{{ route('videos.store') }}">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="_videoId" value="{{ $video->id }}">
+                                    <input type="hidden" name="_userId" value="{{ Auth::user()->id }}">
+                                    <input type="hidden" name="_userIp" value="{{ $_SERVER['REMOTE_ADDR'] }}">
+                                    <button class="btn btn-default btn-xs" type="submit" name="_opinion" value="1" @if(\App\Models\Vote::votesLeft() == 0) disabled @endif><i class="fa fa-fw fa-thumbs-up"></i> Upvote</button>
+                                    <button class="btn btn-default btn-xs" type="submit" name="_opinion" value="-1" @if(\App\Models\Vote::votesLeft() == 0) disabled @endif><i class="fa fa-fw fa-thumbs-down"></i> Downvote</button>
+                                </form>
+                                <hr>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
