eb9d23b3b2218afee62115a4097568d323f417bf
Todd Austin <austin.todd.j@gmail.com>
2017-04-26 12:11:52 -0500

    Order all videos by created_at date

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -7,9 +7,9 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">All Videos ({{ sizeof($data->data) }})</div>
+                    <div class="panel-heading">All Videos ({{ sizeof($data) }})</div>
                     <div class="panel-body">
-                        @foreach($data->data as $video)
+                        @foreach($data as $video)
                             <p><a href="{{ $video->attributes->url }}" target="_blank"> {{ $video->attributes->title }}</a> <span class="text-muted small"><i class="fa fa-fw fa-eye"></i> {{ $video->attributes->view_tally }}&nbsp;&nbsp;<i class="fa fa-fw fa-check"></i> {{ $video->attributes->vote_tally }}</span></p>
                             <form role="form" method="POST" action="{{ route('videos.store') }}">
                                 {{ csrf_field() }}
